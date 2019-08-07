ad0b9ba9211f2991afa36165bece448558714bfa
rplenzdorf <53168242+rplenzdorf@users.noreply.github.com>
2019-08-07 08:30:37 +0200

    Umbau Main Anfang wirbelstr.

--- main.m
+++ main.m
@@ -6,6 +6,7 @@ pruef = true;
 global Nx;
 global sv;
 global Re;
+global obj;
 
 while pruef == true
     Nx = input("wie groß soll die Auflösung in X-Richtung sein?\n")
@@ -14,7 +15,16 @@ while pruef == true
     
     if a==1
         b = input("welches Objekt soll umströmt werden?\n \n 1) Kreis\n 2) Rechteck\n 3) eigenes Objekt\n")
-        disp("obj")
+        if b == 1
+            obj = (x-.5).^2+((y -.5)).^2<.2^2;
+            wirbelstr(
+        elseif b == 2
+            obj = (x-.5).^10+((y -.5)).^10<.2^10;
+        elseif b == 3
+            obj = input("Objekt: \n")
+        else
+            disp("ungültige Eingabe")
+        end
         pruef = false;
     elseif a == 2
         Re = input("bei welcher Reynoldszahl sol die Strömung simuliert werden? \n")
